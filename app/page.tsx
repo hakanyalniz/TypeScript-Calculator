f2aeaf13036bd15c038a01ddfb1a7828a486b6e6
hakanyalniz <hakan_yalniz@live.com>
2025-01-21 19:44:18 +0300

    Added colors to the calculator, buttons and so on.

--- app/page.tsx
+++ app/page.tsx
@@ -2,38 +2,69 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <div
-        className="border-2 border-white h-[375px] w-[244px]"
+        className="border-2 border-white h-[395px] w-[244px]"
         id="calculator-container"
       >
-        <div className="border-2 border-red-400 h-full" id="calculator-inner">
+        <div
+          className="border-2 border-red-400 h-full bg-[#1a3942]"
+          id="calculator-inner"
+        >
           <div className="border border-white h-10">Sunlight</div>
           <div className="border-y-8 border-white h-20 bg-black pt-8 pr-2 text-right">
             Screen
           </div>
-          <div className="button-pad font-[family-name:var(--font-nova-mono)]">
-            <button className="border rounded m-2 py-1 px-10 bg-red-700">
+          <div className="button-pad font-[family-name:var(--font-nova-mono)] bg-[#407c8f]">
+            <button className="border-b-4 border-[#3f0e0e] rounded m-2 py-1 px-10 bg-red-700">
               AC
             </button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">/</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">x</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">7</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">8</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">9</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">-</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">4</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">5</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">6</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">+</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">1</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">2</button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">3</button>
-            <button className="border rounded m-2 py-[1.8rem] px-4 bg-blue-700 absolute">
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              /
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              x
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              7
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              8
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              9
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              -
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              4
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              5
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              6
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              +
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              1
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              2
+            </button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              3
+            </button>
+            <button className="border-b-4 border-[#0f1446] rounded m-2 py-[1.8rem] px-4 bg-blue-700 absolute">
               =
             </button>
-            <button className="border rounded m-2 py-1 px-[2.8rem] bg-black">
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-[2.8rem] bg-black">
               0
             </button>
-            <button className="border rounded m-2 py-1 px-4 bg-black">.</button>
+            <button className="border-b-4 border-[#1d1d1d] rounded m-2 py-1 px-4 bg-black">
+              .
+            </button>
           </div>
         </div>
       </div>
